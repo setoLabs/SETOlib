5a1f0902e2e2b3bba2ab7467ac86e44eabe60599
Sebastian Stenzel <sebastianstenzel@googlemail.com>
2013-02-15 13:12:44 +0100

    Initial Commit

---
+++ SETOlibDemo/SETOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SETOAppDelegate.h
+//  SETOlibDemo
+//
+//  Created by Sebastian Stenzel on 15.02.13.
+//  Copyright (c) 2013 setolab.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SETOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SETOlibDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SETOlibDemo
+//
+//  Created by Sebastian Stenzel on 15.02.13.
+//  Copyright (c) 2013 setolab.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SETOAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SETOAppDelegate class]));
+	}
+}
