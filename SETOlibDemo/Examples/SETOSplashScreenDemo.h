de6674b128a4e88c58bc9a12d63843e46a7b1422
Tobias Hagemann <tobias@hagemann.org>
2013-02-24 16:35:54 +0100

    Changed organization name to setolabs.com in all files.

--- SETOlibDemo/Examples/SETOSplashScreenDemo.h
+++ SETOlibDemo/Examples/SETOSplashScreenDemo.h
@@ -3,7 +3,7 @@
 //  SETOlibDemo
 //
 //  Created by Sebastian Stenzel on 15.02.13.
-//  Copyright (c) 2013 setolab.com. All rights reserved.
+//  Copyright (c) 2013 setolabs.com. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
