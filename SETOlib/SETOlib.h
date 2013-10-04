dac52c955be8b6745b7f20a16024f443853d8518
Sebastian Stenzel <sebastianstenzel@gmail.com>
2013-10-05 00:53:17 +0200

    - added .framework target

---
+++ SETOlib/SETOlib.h
@@ -0,0 +1,13 @@
+//
+//  SETOlib.h
+//  SETOlibDemo
+//
+//  Created by Sebastian Stenzel on 05.10.13.
+//  Copyright (c) 2013 setolabs.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import	<SETOlib/SETOCommon.h>
+#import	<SETOlib/SETOBlockingAlertView.h>
+#import	<SETOlib/SETOSplashScreen.h>
